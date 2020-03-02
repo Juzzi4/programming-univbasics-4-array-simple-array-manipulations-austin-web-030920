1b9b838dc91e2dd071ef9d3b11fdcc5ed58cfb3e
Joshua Nuzzi <jnuzzi47@gmail.com>
2020-03-02 21:16:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,8 +7,7 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-  using_pop = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  antarctica = using_pop.pop
+  antarctica = array.pop
 end
 
 def pop_with_args(array)
