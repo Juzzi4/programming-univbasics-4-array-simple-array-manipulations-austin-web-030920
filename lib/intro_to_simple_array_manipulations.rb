f392fa0ad4b54ea48bea4d52cd7c1279b81f43fd
Joshua Nuzzi <jnuzzi47@gmail.com>
2020-03-02 21:09:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,19 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  using_pop = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  antarctica = using_pop.pop
+end
+
+def pop_with_args(array)
+  pop_with_args = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+ pop_with_args = pop_with_args.pop(2)
+end
+
+def using_shift()
